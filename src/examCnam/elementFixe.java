0b9338b7ea366c8331e8cbbac93f2200273c225a
StraetenMax <maxstraeten@yahoo.com>
2017-05-02 14:02:16 +0200

    finalisationde de la class elementFixe

--- src/examCnam/elementFixe.java
+++ src/examCnam/elementFixe.java
@@ -17,12 +17,31 @@ public class elementFixe extends elementGraphique{
 		positionY = cPositionY;
 	}
 	 //getteur
+	public int getPositionX(){
+		return positionX;
+	}	
 	
-	
-	
-	
-	
-	
-	
+	public int getPositionY(){
+		return positionY;
 	}
+	//setteur
+	public void setPositionX(int pPositionX){
+		positionX=pPositionX;
+	}
+	public void setPositionY(int pPositionY){
+		positionY=pPositionY;
+	}
+	//méthodes
+	public String afficher(){
+		String str= "Elément fixe affiché";
+		return str;
+	}
+	public String decrire(){
+		String str1 = super.decrire()+" qui est en position "+this.getPositionX()+" et position "+this.getPositionY();
+		return str1;
+	}
+	
+	
+	
+	
 }
