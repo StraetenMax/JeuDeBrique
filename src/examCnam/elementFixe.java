97e02da0b67cabc48b0d0b3fe6a885dc0fec35bb
StraetenMax <maxstraeten@yahoo.com>
2017-05-02 13:34:38 +0200

    création de la classe elementFixe

--- src/examCnam/elementFixe.java
+++ src/examCnam/elementFixe.java
@@ -1,5 +1,28 @@
 package examCnam;
 
-public class elementFixe {
-
+public class elementFixe extends elementGraphique{
+	//propriétés
+	private int positionX, positionY;
+	
+	//constructeur vide
+	public elementFixe(){
+		super();
+		positionX = 0;
+		positionY = 0;
+	}
+	//constructeur initialisé
+	public elementFixe(String cNom, int cHauteur, int cLargeur, int cPositionX, int cPositionY){
+		super(cNom, cHauteur, cLargeur);
+		positionX = cPositionX;
+		positionY = cPositionY;
+	}
+	 //getteur
+	
+	
+	
+	
+	
+	
+	
+	}
 }
