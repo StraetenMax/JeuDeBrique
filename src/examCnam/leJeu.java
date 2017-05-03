45ca06fb9649c3b6090cd663afd25d88dc35a3d7
StraetenMax <maxstraeten@yahoo.com>
2017-05-03 13:53:29 +0200

    finalisation de l'exercice

--- src/examCnam/leJeu.java
+++ src/examCnam/leJeu.java
@@ -9,6 +9,13 @@ public class leJeu {
 		brique B2 = new brique("Brique 2", 40, 200, 700, 300, 0);
 		mur M1 = new mur("Mur 1", 40, 20000, 1, 1,"Bleu");
 		mur M2 = new mur("Mur 2", 20000, 40,1, 1,"Bleu");
+	
+		System.out.println(E1.decrire());
+		System.out.println(F1.decrire());
+		System.out.println(B1.decrire());
+		
+		System.out.println(B1.estMoinsAbimeeQue(B2));
+		System.out.println(M1.plusHautQue(M2));
 	}
 
 }
