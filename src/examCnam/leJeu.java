2c99c2c9eba80ba9e6fd100d0d53279f77fcc30f
StraetenMax <maxstraeten@yahoo.com>
2017-05-03 13:39:30 +0200

    création de la classe Main

---
+++ src/examCnam/leJeu.java
@@ -0,0 +1,14 @@
+package examCnam;
+
+public class leJeu {
+
+	public static void main(String[] args) {
+		elementGraphique E1 = new elementGraphique("balle",80,80);
+		elementFixe F1 = new elementFixe("Titre",40,1000,150,1300);
+		brique B1 = new brique("Brique 1", 40, 200, 300, 300, 0);
+		brique B2 = new brique("Brique 2", 40, 200, 700, 300, 0);
+		mur M1 = new mur("Mur 1", 40, 20000, 1, 1,"Bleu");
+		mur M2 = new mur("Mur 2", 20000, 40,1, 1,"Bleu");
+	}
+
+}
